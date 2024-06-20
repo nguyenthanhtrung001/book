de72d19eff1728b80552d255fcadcfb7af3051ca
nguyenthanhtrung001 <thanhtrung8674@gmail.com>
2024-06-20 23:57:52 +0700

    update

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'abc/book-cicd',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '771e0c4269a31ec6f5d9ab70f37064b8ed55475e',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'abc/book-cicd' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '771e0c4269a31ec6f5d9ab70f37064b8ed55475e',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'phpmailer/phpmailer' => array(
+            'pretty_version' => 'v6.9.1',
+            'version' => '6.9.1.0',
+            'reference' => '039de174cd9c17a8389754d3b877a2ed22743e18',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../phpmailer/phpmailer',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
